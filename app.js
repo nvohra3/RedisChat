7c27645a86ca7a7e1070d79d794bd777341d1460
Neil Vohra <nvohra@uga.edu>
2016-07-07 01:12:24 -0700

    Changes from a while ago I never pushed. Small things.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var app_port = process.env.PORT || 8001;
-var REDIS_HOST = "redischatcluster.fqys9c.0001.usw2.cache.amazonaws.com";
-var REDIS_PORT = 6379;
+// var REDIS_HOST = "redischatcluster.fqys9c.0001.usw2.cache.amazonaws.com";
+// var REDIS_PORT = 6379;
 
 require('events').EventEmitter.defaultMaxListeners = Infinity;
 var express = require("express");
@@ -12,8 +12,8 @@ var CookieParser = require('cookie-parser');
 var cookieParser = CookieParser('secret');
 var redis = require('redis');
 var RedisStore = require('connect-redis')(ExpressSession);
-// var rClient = redis.createClient();
-var rClient = redis.createClient(REDIS_PORT, REDIS_HOST);
+var rClient = redis.createClient();
+// var rClient = redis.createClient(REDIS_PORT, REDIS_HOST);
 var redisStore = new RedisStore({client:rClient});
 var ios = require('socket.io-express-session');
 
@@ -29,53 +29,55 @@ app.use(session);
 io.use(ios(session)); // session support
 
 // For local host use
-// var sub = redis.createClient();
-// var pub = redis.createClient();
-var sub = redis.createClient(REDIS_PORT, REDIS_HOST);
-var pub = redis.createClient(REDIS_PORT, REDIS_HOST);
-var CHAT_NEW_USER = "chat.newUser"
-var CHAT_NEW_MESSAGE = "chat.newMessage";
-var CHAT_USER_LEAVING = "chat.userLeaving";
-// How does psubscribe pattern matching work? Could reduce this to one line.
-sub.subscribe(CHAT_NEW_USER);
-sub.subscribe(CHAT_NEW_MESSAGE);
-sub.subscribe(CHAT_USER_LEAVING);
+var sub = redis.createClient();
+var pub = redis.createClient();
+// For AWS host use
+// var sub = redis.createClient(REDIS_PORT, REDIS_HOST);
+// var pub = redis.createClient(REDIS_PORT, REDIS_HOST);
+
+var CHAT = "redis_chat";
+sub.subscribe(CHAT);
 
 app.get("/", function(req, res) {
     res.sendFile("index.html", { root: __dirname });
 });
 
+var NEW_USER_JOINED_ROOM = "userJoined";
+var NEW_MESSAGE = "newMessage";
+var USER_LEFT_ROOM = "userLeaving";
+
 io.on("connection", function(socket) {
     var username = socket.handshake.query.username;
-    console.log(username + " joined the chatroom.");
+    console.log("Socket connection established for " + username +".");
 
     // Notify all subscribers that a new user has joined the chatroom
-    pub.publish(CHAT_NEW_USER, JSON.stringify({ "username" : username }));
+    pub.publish(CHAT, JSON.stringify({ "username" : username, "messageType" : NEW_USER_JOINED_ROOM }));
 
-    socket.on("toServerMessage", function(data) {
-        pub.publish(CHAT_NEW_MESSAGE, JSON.stringify({ "username" : username, "body" : data.body }));
+    socket.on(NEW_MESSAGE, function(data) {
+        pub.publish(CHAT, JSON.stringify({ "username" : username, "body" : data.body, "messageType" : NEW_MESSAGE }));
     });
 
-    socket.on("leavingChatroom", function() {
-        pub.publish(CHAT_USER_LEAVING, JSON.stringify({ "username" : username }));
+    socket.on(USER_LEFT_ROOM, function() {
+        pub.publish(CHAT, JSON.stringify({ "username" : username, "messageType" : USER_LEFT_ROOM }));
     });
 
     sub.on("message", function(channel, data) {
         var message = JSON.parse(data);
-        if (channel.localeCompare(CHAT_NEW_USER) == 0)
+        var messageType = message.messageType;
+        if (messageType.localeCompare(NEW_USER_JOINED_ROOM) == 0)
         {
             var joinedMessage = " has joined the chatroom. Say hello!";
-            console.log(message.username + joinedMessage);
-            socket.emit("newUser", { "username" : message.username, "body" : joinedMessage });
-        } else if (channel.localeCompare(CHAT_NEW_MESSAGE) == 0)
+            console.log(username + " log:\t" + message.username + joinedMessage);
+            socket.emit(NEW_USER_JOINED_ROOM, { "username" : message.username, "body" : joinedMessage });
+        } else if (messageType.localeCompare(NEW_MESSAGE) == 0)
         {
-            console.log(message.username + " just sent the message \"" + message.body + "\"");
-            socket.emit("toUserMessage", { "username" : message.username, "body" : message.body });
-        } else if (channel.localeCompare(CHAT_USER_LEAVING) == 0)
+            console.log(username + " log:\t" + message.username + " sent the message \"" + message.body + "\"");
+            socket.emit(NEW_MESSAGE, { "username" : message.username, "body" : message.body });
+        } else // if (messageType.localeCompare(USER_LEFT_ROOM) == 0)
         {
             var leavingMessage = " has left the chatroom. :(";
-            console.log(message.username + leavingMessage);
-            socket.emit("userLeaving", { "username" : message.username, "body" : leavingMessage });
+            console.log(username + " log:\t" + message.username + leavingMessage);
+            socket.emit(USER_LEFT_ROOM, { "username" : message.username, "body" : leavingMessage });
         }
     });
 });
